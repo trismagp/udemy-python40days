e880690da92ef956267967c05a84f188d8bf6c15
trismagp <tris.magp@gmail.com>
2023-01-08 20:37:52 +0100

    implement checkbox for completing a todo

--- Day19-todo-streamlit/web.py
+++ Day19-todo-streamlit/web.py
@@ -12,8 +12,12 @@ st.title('My todo app')
 st.subheader('this is my todo app')
 st.write('this app is awesome')
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        complete_todo(todo)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", 
             placeholder="Add new todo...",
